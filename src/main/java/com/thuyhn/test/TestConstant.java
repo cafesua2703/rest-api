e058c275967271a146622859dbd6d4b3101ace09
cafesua2703 <cafesua2703@gmail.com>
2020-08-09 16:45:05 +0700

    Refactor code

---
+++ src/main/java/com/thuyhn/test/TestConstant.java
@@ -0,0 +1,20 @@
+package com.thuyhn.test;
+
+
+public class TestConstant {
+    public static final int _18 = 18;
+    public static final int _17 = 17;
+    public static final int _15 = 15;
+    public static final int _14 = 14;
+    public static final int _13 = 13;
+    public static final int _12 = 12;
+    public static final int _11 = 11;
+    public static final int _8 = 8;
+    public static final int _9 = 9;
+    public static final int _7 = 7;
+    public static final int _6 = 6;
+    public static final int _5 = 5;
+    public static final int _3 = 3;
+    public static final int _2 = 2;
+    public static final int _1 = 1;
+}
